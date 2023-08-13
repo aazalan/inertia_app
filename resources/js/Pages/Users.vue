e588b596db7bef96673668f0d23bfe7e69cffd0e
Alina Abdul <alinka140901@gmail.com>
2023-08-13 22:48:16 +0300

    add pagination

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,16 +1,32 @@
 <template>
     <Head title="Users" />
     <h1 class="text-3xl">Users</h1>
-    <ul>
-        <li
-        v-for="user in users"
-        :key="user.id"
-        v-text="user.name"></li>
-    </ul>
+
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="user in users.data" :key="user.id">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" v-text="user.name">
+                    </th>
+                    <td class="px-6 py-4">
+                        <Link :href="`users/${user.id}/edit`"
+                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                            Edit
+                        </Link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Paginator -->
+    <pagination :links="users.links" class="mt-6"></pagination>
+
 </template>
 
 <script setup>
-    defineProps({ users : Array })
+    import Pagination from '../Shared/Pagination.vue';
+    defineProps({ users : Object });
 </script>
 
 <style>
---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,24 @@
+<template>
+      <div>
+        <Component 
+        class="px-1"
+        :class="{'text-gray-500' : ! link.url, 'font-bold' : link.active}"
+        v-for="link in links" 
+        :is="link.url? 'Link' : 'span'"
+        :key="link.label" 
+        :href="link.url" 
+        v-html="link.label"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        links: Array
+    }
+}
+</script>
+
+<style>
+
+</style>
